1e6f4227f5a7710f4f838299304f3e82cf2889c7
Julia De Jesus <julsvdj01@gmail.com>
2023-06-12 12:08:35 +0800

    june 12th commit

--- calendars_start.py
+++ calendars_start.py
@@ -3,17 +3,44 @@
 # import calendar module
 import calendar
 
-# Create a plain text calendar
-
+"""""
+Create a plain text calendar
+"""""
+# c = calendar.TextCalendar(calendar.MONDAY)
+# st = c.formatmonth(2023, 1, 0, 0)
+# print(st)
 
 # Create an HTML formatted calendar
-
+hc = calendar.HTMLCalendar(calendar.SUNDAY)
+st = hc.formatmonth(2023, 1)
+# print(st)
 
 # loop over days of a month
 # zeroes mean that the day of the week is in an overlapping month
+# for i in hc.itermonthdays(2023,8) :
+#     print(i)
 
+''''
+The Calendar module provides useful utilities for the given locale, usch as the names of days and months in both full and abbreviated forms
+'''
+# for name in calendar.month_name: 
+#     print(name)
 
-# The Calendar module provides useful utilities for the given locale, usch as the names of days and months in both full and abbreviated forms
+# for day in calendar.day_name: 
+#     print(day)
 
+''''
+Calculate days based on a rule. For example, consider a team meeting on the first Fridat of every month. To figure out what days that would be for each month, we can use this script: 
+'''
+print("Team Meetings will be on: ")
+for m in range(1,13): 
+    cal = calendar.monthcalendar(2023, m)
+    weekone = cal[0]
+    weektwo = cal[1]
 
-# Calculate days based on a rule. For example, consider 
+    if weekone[calendar.FRIDAY] != 0: 
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+    
+    print("%10s %2d" % (calendar.month_name[m], meetday))
---
+++ file_start.py
@@ -0,0 +1,20 @@
+def main():
+    # Open a file for writing and create it if it does not exist
+    # f = open("textfile.txt", "w+")
+
+    # Open the file for appending text to the end
+    f = open("textfile.txt", "a")
+
+    # Write some lines of data to the file
+    for i in range(10): 
+            f.write("This is line " + str(i) + "\r\n")
+
+    # Close the file when done 
+    f.close()
+
+    # Open the file back up and read the contents
+
+
+if __name__ == "__main__":
+    main() 
+
