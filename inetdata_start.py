debf0029daae50cb039bc9f24dbdadf746beb942
Julia De Jesus <julsvdj01@gmail.com>
2023-06-28 14:33:25 +0800

    Finished Python Tutotial in LinkedIn

--- inetdata_start.py
+++ inetdata_start.py
@@ -1,5 +1,11 @@
 # Example File for retrieving data from the internet
+import urllib.request
 
+def main():
+    webURL = urllib.request.urlopen('https://www.google.com')
+    print("result code: " + str(webURL.getcode()))
+    data = webURL.read()
+    print(data)
 
 
 
